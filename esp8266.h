f2f638aab639f6e46077b57b680752653544be2a
Shi Warai <zhuravlev.p.r@edu.mirea.ru>
2022-05-12 19:51:18 +0300

    Build: init library

---
+++ esp8266.h
@@ -0,0 +1,64 @@
+#ifndef ESP8266_H
+#define ESP8266_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#define ESP_BUFFER_SIZE 512
+
+// Answers of AT commands
+typedef enum {
+	OK_RESPONSE,
+	ERROR_RESPONSE
+} BASE_RESPONSE;
+
+#ifndef BASE_CASES
+static char* BASE_CASES[] = {"OK\r\n", "ERROR\r\n"};
+#endif
+// End
+
+// Modes and function AT+CWMODE
+typedef enum {
+	STATION_MODE,
+	SOFT_AP_MODE,
+	BOTH_MODE
+} WIFI_MODE;
+
+BASE_RESPONSE esp_set_wifi_mode(WIFI_MODE mode);
+//
+
+// Structure and functions AT+CIPSTA
+typedef struct {
+	char ip[16];
+	char gateway[16];
+	char netmask[16];
+} NET_STATS;
+
+BASE_RESPONSE esp_get_station_netstats(NET_STATS* stats);
+BASE_RESPONSE esp_set_ip(NET_STATS config);
+// End
+
+void esp_init();
+BASE_RESPONSE esp_test();
+BASE_RESPONSE esp_connect_to_wifi(char* ssid, char* pswd);
+
+// TCP/UDP functions
+typedef enum {
+	AS_CLIENT_STATUS,
+	AS_SERVER_STATUS,
+	GOT_IP_STATUS,
+	CONNECTED_STATUS,
+	DISCONNECTED_STATUS
+} CONNECTION_STATUS;
+
+CONNECTION_STATUS esp_connection_status();
+BASE_RESPONSE esp_create_connection(char type[], char ip[], char port[]);
+BASE_RESPONSE esp_close_connection();
+BASE_RESPONSE esp_send_data(char data[], size_t size);
+BASE_RESPONSE esp_get_data(char data[], size_t size, uint32_t timeout);
+
+BASE_RESPONSE esp_send_request(char ip[], char port[], char request[], char data[], size_t size);
+// End
+
+#endif
